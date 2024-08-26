eabbb7eb92b15333fa50fae71fa6b5f38b2e6e1a
Lergio <56664131+Lergio@users.noreply.github.com>
2024-08-25 21:02:13 -0300

    Update NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import '../styles/NotFound.css'
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 - Página No Encontrada</h1>
-      <p>Lo sentimos, la página que estás buscando no existe.</p>
+    <div className="not-found-container">
+      <h1 className="not-found-title">404 - Página No Encontrada</h1>
+      <br></br>
+      <img src="https://cdn-icons-png.freepik.com/512/1698/1698558.png" alt="Página no encontrada" className="not-found-image" />
+      <br></br>
+      <p className="not-found-message">¡Vaya! Has llegado a una página que ni siquiera nosotros conocíamos.</p>
     </div>
   );
 };
